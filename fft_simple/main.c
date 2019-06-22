400a8a7ba104f6d535ebc294b32821c404a35da4
Jakub Hladik <jakeh12@me.com>
2019-06-22 09:19:32 -0500

    Initial Commit

---
+++ fft_simple/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fft_simple
+//
+//  Created by Jakub Hladik on 6/22/19.
+//  Copyright © 2019 Jakub Hladik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
