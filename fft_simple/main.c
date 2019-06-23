8e430cf72aad9f3f637a4c0d4d9c240c42d3c800
Jakub Hladik <jakeh12@me.com>
2019-06-22 20:59:45 -0500

    working hard-coded butterfly structure for 8-point radix-2 dit fft

--- fft_simple/main.c
+++ fft_simple/main.c
@@ -8,42 +8,95 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <complex.h>
 
-
-void twiddle_factor_lookup_table(int n, double twiddle_lut_r[], double twiddle_lut_i[])
+void generate_twiddle_factor_lookup_table(int n, complex double twiddle_lut[])
 {
-    double theta;
-    int i;
-    for (i = 0; i < n/2; i++) {
-        theta = ((-2 * M_PI) / n) * i;
-        twiddle_lut_r[i] = cos(theta);
-        twiddle_lut_i[i] = sin(theta);
+    int r;
+    for (r = 0; r < n; r++) {
+        twiddle_lut[r] = cexp(((-I * 2 * M_PI) / n) * r);
     }
 }
 
-void butterfly(double x0_r, double x0_i, double x1_r, double x1_i, double w_r, double w_i) {
-    //printf("inputs:\nx0: %.4f%+.4fi\tx1: %.4f%+.4fi\tw: %.4f%+.4fi\n", x0_r, x0_i, x1_r, x1_i, w_r, w_i);
-    double y0_r, y0_i, y1_r, y1_i, x0_r_minus_x1_r, x0_i_minus_x1_i;
-    // calculate y0 (complex y0 = x0 + x1)
-    y0_r = x0_r + x1_r;
-    y0_i = x0_i + x1_i;
-    // calculate y1 (complex y1 = w * (x0 - x1))
-    x0_r_minus_x1_r = x0_r - x1_r;
-    x0_i_minus_x1_i = x0_i - x1_i;
-    y1_r = w_r * x0_r_minus_x1_r - w_i * x0_i_minus_x1_i;
-    y1_i = w_r * x0_i_minus_x1_i + w_i * x0_r_minus_x1_r;
-    //printf("outputs:\ny0: %.4f%+.4fi\ty1: %.4f%+.4fi\n\n", y0_r, y0_i, y1_r, y1_i);
+void butterfly(complex double x0, complex double x1, complex double wk, complex double *y0, complex double *y1) {
+    *y0 = x0 + wk * x1;
+    *y1 = x0 - wk * x1;
 }
 
 int main(int argc, const char * argv[]) {
     int n = 8; // n has to be a power of two
-    double twiddle_lut_r[n], twiddle_lut_i[n];
-    twiddle_factor_lookup_table(n, twiddle_lut_r, twiddle_lut_i);
-    int i;
-    for (i = 0; i < n/2; i++) {
-            printf ("%+.10f%+.10fi\n", twiddle_lut_r[i], twiddle_lut_i[i]);
+    complex double twiddle_lut[n];
+    generate_twiddle_factor_lookup_table(n, twiddle_lut);
+    printf("twiddle factors:\n");
+    int r;
+    for (r = 0; r < n; r++) {
+        printf("%+f%+fi\n", crealf(twiddle_lut[r]), cimagf(twiddle_lut[r]));
     }
     
-    butterfly(1.224242, 0.113198, 5.422113, -0.411351, 0.00113141, -0.948824);
+    complex double input[n];
+    input[0] = +0.0 + 0.0*I;
+    input[4] = +0.0 + 0.0*I;
+    input[2] = +1.0 + 0.0*I;
+    input[6] = +1.0 + 0.0*I;
+    input[1] = +0.0 + 0.0*I;
+    input[5] = +0.0 + 0.0*I;
+    input[3] = +1.0 + 0.0*I;
+    input[7] = +1.0 + 0.0*I;
+    
+    // stage 0
+    complex double y0_0_0, y1_0_0;
+    butterfly(input[0], input[4], twiddle_lut[0], &y0_0_0, &y1_0_0);//
+    complex double y0_0_1, y1_0_1;
+    butterfly(input[2], input[6], twiddle_lut[0], &y0_0_1, &y1_0_1);//
+    complex double y0_0_2, y1_0_2;
+    butterfly(input[1], input[5], twiddle_lut[0], &y0_0_2, &y1_0_2);//
+    complex double y0_0_3, y1_0_3;
+    butterfly(input[3], input[7], twiddle_lut[0], &y0_0_3, &y1_0_3);//
+
+    // stage 1
+    complex double y0_1_0, y1_1_0;
+    butterfly(y0_0_0, y0_0_1, twiddle_lut[0], &y0_1_0, &y1_1_0);//
+    complex double y0_1_1, y1_1_1;
+    butterfly(y1_0_0, y1_0_1, twiddle_lut[2], &y0_1_1, &y1_1_1);//
+    complex double y0_1_2, y1_1_2;
+    butterfly(y0_0_2, y0_0_3, twiddle_lut[0], &y0_1_2, &y1_1_2);//
+    complex double y0_1_3, y1_1_3;
+    butterfly(y1_0_2, y1_0_3, twiddle_lut[2], &y0_1_3, &y1_1_3);//
+    
+    // stage 2
+    complex double y0_2_0, y1_2_0;
+    butterfly(y0_1_0, y0_1_2, twiddle_lut[0], &y0_2_0, &y1_2_0);//
+    complex double y0_2_1, y1_2_1;
+    butterfly(y0_1_1, y0_1_3, twiddle_lut[1], &y0_2_1, &y1_2_1);//
+    complex double y0_2_2, y1_2_2;
+    butterfly(y1_1_0, y1_1_2, twiddle_lut[2], &y0_2_2, &y1_2_2);//
+    complex double y0_2_3, y1_2_3;
+    butterfly(y1_1_1, y1_1_3, twiddle_lut[3], &y0_2_3, &y1_2_3);//
+    
+    complex double output[n];
+    output[0] = y0_2_0;
+    output[1] = y0_2_1;
+    output[2] = y0_2_2;
+    output[3] = y0_2_3;
+    output[4] = y1_2_0;
+    output[5] = y1_2_1;
+    output[6] = y1_2_2;
+    output[7] = y1_2_3;
+    
+    printf("input vector:\n");
+    printf("%+f%+fi\n", crealf(input[0]), cimagf(input[0]));
+    printf("%+f%+fi\n", crealf(input[4]), cimagf(input[4]));
+    printf("%+f%+fi\n", crealf(input[2]), cimagf(input[2]));
+    printf("%+f%+fi\n", crealf(input[6]), cimagf(input[6]));
+    printf("%+f%+fi\n", crealf(input[1]), cimagf(input[1]));
+    printf("%+f%+fi\n", crealf(input[5]), cimagf(input[5]));
+    printf("%+f%+fi\n", crealf(input[3]), cimagf(input[3]));
+    printf("%+f%+fi\n", crealf(input[7]), cimagf(input[7]));
+
+    printf("output vector:\n");
+    for (r = 0; r < n; r++) {
+        printf("%+f%+fi\n", crealf(output[r]), cimagf(output[r]));
+    }
+    
     return 0;
 }
